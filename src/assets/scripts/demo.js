9eeb702dff9fdbef5f21fc40c9c5299d49ca3219
AnilaMilton <anilamilton1997@gmail.com>
2019-07-21 16:25:16 +0530

    weekend task

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,64 +1,28 @@
-var $window = $(window);
-    $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-        $(window).scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        });
-    });
+// console.log('Demo!');
 
-    $(window).scroll(function(){
-      var scroll = $(window).scrollTop();
-      if (scroll > 0) {
-        $(".navbar").addClass("navbar-scroll");
-      }
-      else{
-        $(".navbar").removeClass("navbar-scroll");
-      }
-      if (scroll > 200) {
-        $(".navbar").addClass("bg-primary");
-      }
-      else{
-        $(".navbar").removeClass("bg-primary");
-      }
-    })
-  
-    $(".toggle-accordion").on("click", function() {
-      var accordionId = $(this).attr("accordion-id"),
-        numPanelOpen = $(accordionId + ' .collapse.in').length;
-  
-      $(this).toggleClass("active");
-  
-      if (numPanelOpen == 0) {
-        openAllPanels(accordionId);
-      } else {
-        closeAllPanels(accordionId);
-      }
-    })
-  
-    openAllPanels = function(aId) {
-      console.log("setAllPanelOpen");
-      $(aId + ' .panel-collapse:not(".in")').collapse('show');
-    }
-    closeAllPanels = function(aId) {
-      console.log("setAllPanelclose");
-      $(aId + ' .panel-collapse.in').collapse('hide');
-    }
-  
-    // Add minus icon for collapse element which is open by default
-    $(".collapse.show").each(function(){
-      $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-      });
-      // Toggle plus minus icon on show hide of collapse element
-      $(".collapse").on('show.bs.collapse', function(){
-      $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
-      }).on('hide.bs.collapse', function(){
-      $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
-      });
+// $(".btn").click(function(){
+//   $(".btn").toggleClass("minu");
+// });
 
+$(window).scroll(function(e){
+    parallax();
+  });
 
+// const $navbar = $(".nav"),y_pos = $navbar.offset().top,height = $navbar.height();
 
+// //scroll top 0 sticky
+// $(document).scroll(function() {
+//   let scrollTop = $(this).scrollTop();
+//   if (scrollTop > 0) {
+//     $navbar.addClass("sticky");
+//   } else {
+//     $navbar.removeClass("sticky");
+//   }
+// });
 
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.jumbotron__container').css('top',-(scrolled*0.0315)+'rem');
+    // $('.hero > .hero__text > h1').css('top',-(scrolled*-0.005)+'rem');
+    // $('.hero > .hero__text > h1').css('opacity',1-(scrolled*.00175));
+  };
